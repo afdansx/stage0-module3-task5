13e9e9d8b493a13b3f3ebe41aa769eaa899fbb7d
afdansx <dd.naidenko@gmail.com>
2023-11-23 00:09:51 +0200

    solved

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+
+    void divide() {
+        int divider = 5;
+        int num = 19;
+        System.out.println(num % divider);
+
+
     }
 }
--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first = 1;
+        String str = String.format("%d", first);
+        int second = 10;
+        String str1 = String.format("%d", second);
+        int third = 100;
+        String str2 = String.format("%d", third);
+        System.out.println(str + str1 + str2);
     }
 }
